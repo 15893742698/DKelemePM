d6a685e3eb16f4167f7b10c36316949062401397
deng <2524024789@qq.com>
2018-11-02 09:30:31 +0800

    增加订单

--- my-project/src/components/order.vue
+++ my-project/src/components/order.vue
@@ -4,6 +4,20 @@
           <img src="../imgs/后退.png" alt="" @click="returnuup">
           <p>{{$store.state.titlename}}</p>
         </div>
+        <div class="ord-one"> 
+          <div class="ord-two">
+            <img src="./img/减.png" alt="">
+            <div> 
+            <p>两万五v</p>
+            <p>shuiguo1-默认</p>
+            </div>
+          </div> 
+          <div class="ord-three">
+            <p>支付成功</p>
+            <p style="color: orangered; font-size: .2rem; float:right;">20</p>
+            <p>再来一单</p>
+          </div>
+          </div>
         <Botfix></Botfix>
     </div>    
 </template>
@@ -49,6 +63,30 @@ export default {
 };
 </script>
 <style>
+.ord-one{
+  display: flex;
+  padding: .2rem .04rem;
+  justify-content:space-between; 
+}
+.ord-two{
+  display: flex;
+  justify-content: space-between;
+  
+}
+.ord-two>img{
+  width: .4rem;
+  height: .4rem;
+}
+.ord-two p{
+  font-size: .15rem;
+  margin-top: .16rem;
+}
+.ord-three p{
+  font-size: .23rem;
+}
+.ord-three p:nth-child(3){
+  clear:both;
+}
 .orderdiv {
   width: 100%;
 }
