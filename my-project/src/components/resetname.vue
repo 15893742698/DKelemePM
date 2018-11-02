4494db0bc8a0d06df5fc3e549e888aa4327631ac
Sipengju <251125595@qq.com>
2018-11-02 16:13:32 +0800

    no message

--- my-project/src/components/resetname.vue
+++ my-project/src/components/resetname.vue
@@ -6,6 +6,11 @@
             <p>{{resetnametips}}</p>
             <button @click="rename33" :disabled="res">确认修改</button>
         </div>
+        <div class="songzi" v-if="songzi">
+            <img src="../imgs/感叹.png" alt="">
+            <p>{{failmsg}}</p>
+            <button @click="godenglu">确认</button>
+        </div>
     </div>
 </template>
 <script>
@@ -21,7 +26,9 @@ export default {
       rename: "",
       res: Boolean,
       resetnametips: "用户名只能修改一次(5-24字符之间)",
-      loading: true
+      loading: true,
+      failmsg: "",
+      songzi: false
     };
   },
   created() {
@@ -38,16 +45,26 @@ export default {
   methods: {
     jtinput() {
       var reg = /\w{5,24}/;
-      this.res = reg.test(this.rename);
-      if (!this.res) {
-        this.res = true;
+      var res = reg.test(this.rename);
+      if (!res) {
+        res = true;
         this.resetnametips = "用户名长度在5到24位之间";
       } else {
-        this.res = false;
+        res = false;
         this.resetnametips = "用户名只能修改一次(5-24字符之间)";
       }
     },
     rename33() {
+      var reg = /\w{5,24}/;
+      var res = reg.test(this.rename);
+      this.songzi = true;
+      if (!res) {
+        this.failmsg = "请重新输入用户名";
+      } else {
+        this.failmsg = "修改用户名成功";
+      }
+    },
+    godenglu() {
       this.$store.state.usermsg.username = this.rename;
       this.$router.push({ name: "home" });
     }
@@ -86,6 +103,32 @@ export default {
 }
 .redd {
   border: 1px solid red;
+}
+.songzi {
+  width: 90%;
+  padding: 5%;
+  text-align: center;
+  z-index: 3;
+  position: fixed;
+  top: 25%;
+  background-color: rgb(192, 251, 192);
+}
+.songzi > img {
+  width: 30%;
+}
+.songzi > p {
+  font-size: 0.16rem;
+  padding: 3%;
+}
+.songzi > button {
+  font-size: 0.2rem;
+  height: 0.5rem;
+  background-color: rgb(39, 175, 39);
+  color: white;
+  border-radius: 10px;
+  line-height: 0.5rem;
+  padding: 2%;
+  width: 96%;
 }
 </style>
 
